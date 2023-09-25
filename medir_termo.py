32e37c3931adf61774d466c742694f8c93882ae3
Martin Palomenque <martinpalomenque07@gmail.com>
2023-09-25 10:32:05 -0300

    modifico medir_termo

--- medir_termo.py
+++ medir_termo.py
@@ -2,18 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pyvisa as visa
+import time
 
 rm = visa.ResourceManager()
-print(rm.list_resources())
 multi = rm.open_resource('GPIB0::23::INSTR')
 
 try:
 
-    print(multi.query('*IDN?'))
+    t = []  # tiempo
+    V = []  # voltaje
+    duracion = 5  # s
 
-    dc = multi.query_ascii_values('MEASURE:VOLTAGE:DC?')[0]
-
-    print(f'{dc} V')
+    t0 = time.time()
+    while time.time() - t0 <= duracion:
+        t.append(time.time() - t0)
+        dc = multi.query_ascii_values('MEASURE:VOLTAGE:DC?')[0]
+        V.append(dc)
+    
+    data_termo = pd.DataFrame({'Tiempo [s]' : t, 'Tension [V]' : V})
+    data_termo.to_csv('data_termo.csv', index=False)
 
 
 except:
