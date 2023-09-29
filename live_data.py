b773f371ef5e19726837757d392d95668de1c956
Lucio Coronato <63166416+Luciocoro@users.noreply.github.com>
2023-09-29 20:59:13 -0300

    corregido pequeños errores

--- live_data.py
+++ live_data.py
@@ -19,10 +19,10 @@ def animate(i,t,T,t0):
     t.append(time.time()-t0)
     medicion_V = multitermo.query_ascii_values('MEASURE:VOLTAGE:DC?')[0]
     medicion_V = medicion_V*1e3
-    medicion_T = v2k(medicion_V)
+    medicion_T = v2k(medicion_V) - 273.15
     T = np.append(T,[medicion_T])
     t = t[-200:]  #Para que el plot pueda correr por mucho tiempo
-    T = T[-200:]
+    T = T[-200:] 
 
     line.set_ydata(T)
 
@@ -33,19 +33,14 @@ try:
     # MEDIR
 
     t = np.array([])  # s
-    V = np.array([])  # V
-    R = np.array([])
     T = np.array([])  # K
     duracion = 10  # s
     T_amb = 20 + 273.15  # K
     T_range = 70  # C
+    
     line, = ax.plot(t,T)
 
     t0 = time.time()
-    t = np.array(t)  # s
-    V = np.array(V) * 1e3  # mV
-    T = v2k(V)  # K
-    R = np.array(R)
     ani = animation.FuncAnimation(fig,
                                   animate,
                                   fargs = (t,T,t0),
