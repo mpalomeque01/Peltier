0aca34359d3ea54269d4c1a174329f2c54148e24
Martin Palomenque <martinpalomenque07@gmail.com>
2023-10-03 14:51:05 -0300

    organizo todo

--- metodos_labo.py
+++ metodos_labo.py
@@ -21,38 +21,48 @@ def save(df, filename, path='.'):
 
 cal_pt100 = 1 / 0.385  # K/Ohm
 
-def R2C(R):
-    return (R - 100)*cal_pt100
+R2C = lambda R: (R - 100)*cal_pt100
+
+# incerteza lectura-rango resistencia
+reading_100Ohm  =   0.00008
+range_100Ohm    =   0.00004
+range_1kOhm     =   0.00001
+# Incerteza lectura-rango tension
+reading_1V      =   0.00003
+range_1V        =   0.000007
 
 
 # ESTADISTICA
-
-def lineal(x, a1, a2):
-    return a1 + a2*x
 
 def least_squares(f, x, y, sigma):
     
     n = len(x)  # Numero de mediciones
     k = f.__code__.co_argcount - 1  # Numero de parametros
     parametros = f.__code__.co_varnames[1:k+1]
-
-
+    
+    
     if len(np.array(sigma).shape) == 1:
         sigma = np.identity(len(x))*np.array(sigma)
-
+    
     f = f(sp.symbols('x'), *sp.symbols(parametros))
-
+    
     A = []
     for parametro in sp.symbols(parametros):
-        fprima_i = sp.lambdify('x', sp.diff(f, parametro).simplify())
-        A.append(fprima_i(x))
+        fprima_i = sp.diff(f, parametro).simplify()
+        if 'x' in str(fprima_i):
+            fprima_i = sp.lambdify('x', fprima_i)
+            A.append(fprima_i(x))
+        else:
+            fprima_i = float(fprima_i)
+            A.append(np.array(n*[fprima_i]))
+    
     A = np.array(A).T
-
+    
     y = np.array([y]).T
     pcov = np.linalg.inv(A.T @ np.linalg.inv(sigma) @ A)
     popt = pcov @ A.T @ np.linalg.inv(sigma) @ y
-
+    
     chi_sq = (y - A @ popt).T @ np.linalg.inv(sigma) @ (y - A @ popt)
     P = 1 - chi2.cdf(chi_sq[0,0], n-k)
-
+    
     return popt.T[0], pcov, P
